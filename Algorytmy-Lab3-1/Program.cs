4a50d43abd3b5b9acb5892907ee5a533c9a0124b
oSsulla <osa2000osa@gmail.com>
2022-10-11 17:01:54 +0200

    Dodaj pliki projektów.

---
+++ Algorytmy-Lab3-1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace AlgorytmyLab1
+{
+    public class Lab1
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr1 = { 23, 1, 56, 345, 1, 5, 67, 11 };
+            int index = FindTwoDigItMin(arr1);
+            if (index == 7)
+            {
+                Console.WriteLine("Ok");
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
+            int[] arr2 = { };
+            index = FindTwoDigItMin(arr2);
+            if (index == -1)
+            {
+                Console.WriteLine("Ok");
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
+            int[] arr3 = { 1, 3, 4, 123, 1234 };
+            index = FindTwoDigItMin(arr3);
+            if (index == -1)
+            {
+                Console.WriteLine("Ok");
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
+        }
+
+        public static int FindTwoDigItMin(int[] arr)
+        {
+
+            return 0;
+        }
+    }
+}
